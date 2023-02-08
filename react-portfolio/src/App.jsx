7bbbd2106d0ed89b1aa1e7558f89ebdce4d8ce3c
jypara-git <zhyparaabdieva@gmail.com>
2023-02-07 16:52:22 -0800

    completed the floating nav component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
     <Nav />
     <About />
     <Experience />
+    <Portfolio />
     <Contact />
     <Footer />
     </>
--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {BiHome} from 'react-icons/bi'
+import {BiUser} from 'react-icons/bi'
+import {MdWorkOutline} from 'react-icons/md'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
+import {MdOutlineContactPhone} from 'react-icons/md'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><BiHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><BiUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}><MdWorkOutline/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === "#portfolio" ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><MdOutlineContactPhone/></a>
+    </nav>
   )
 }
 
